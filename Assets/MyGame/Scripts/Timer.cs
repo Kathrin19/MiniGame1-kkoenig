b0f2407765cc79c6f7308d86c24f512ab199cbed
kathrin <kathrin.koenig@htl-salzburg.ac.at>
2019-05-28 13:54:49 +0200

    Added Timer

--- Assets/MyGame/Scripts/Timer.cs
+++ Assets/MyGame/Scripts/Timer.cs
@@ -1,7 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
+    public int timeLeft = 60;
+    public TextMeshProUGUI countdown;
+
+    void Start()
+    {
+        StartCoroutine("LoseTime");
+        Time.timeScale = 1;
+    }
+    void Update()
+    {
+        countdown.text = (" " + timeLeft);
+        if (timeLeft == 0)
+        {
+            SceneManager.LoadScene("End");
+        }
+    }
+
+    IEnumerator LoseTime()
+    {
+        while (timeLeft > 0)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+        }
+    }
 }
