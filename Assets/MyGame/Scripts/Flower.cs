b8fcad8156a751e1b4c546e23da10d56e13643be
kathrin <kathrin.koenig@htl-salzburg.ac.at>
2019-06-16 12:25:23 +0200

    refactoring: magic string flower

--- Assets/MyGame/Scripts/Flower.cs
+++ Assets/MyGame/Scripts/Flower.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Flower : MonoBehaviour {
+    public string bottomCollider = "BottomCollider";
+    public string player = "Player";
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "BottomCollider")
+        if (collision.gameObject.name == bottomCollider)
         {
             Destroy(gameObject);
         }
 
-        if (collision.gameObject.name == "Player")
+        if (collision.gameObject.name == player)
         {
             Destroy(gameObject);
         }
