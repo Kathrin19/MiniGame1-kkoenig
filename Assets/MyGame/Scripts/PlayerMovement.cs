a2192d9787e7eaca4d864e11c7adf3cf597fff5b
kathrin <kathrin.koenig@htl-salzburg.ac.at>
2019-06-16 12:56:41 +0200

    refactoring: magical value PlayerMovement

--- Assets/MyGame/Scripts/PlayerMovement.cs
+++ Assets/MyGame/Scripts/PlayerMovement.cs
@@ -6,11 +6,13 @@ public class PlayerMovement : MonoBehaviour
 {
     private const string AXISHORIZONTAL = "Horizontal";
     private float moveSpeed = 6.0f;
+    private float leftBoundary = -5.6f;
+    private float rightBoundary = 5.7f;
 
     private void Move()
     {
         var deltaX = Input.GetAxis(AXISHORIZONTAL) * Time.deltaTime * moveSpeed;
-        var newPosX = Mathf.Clamp(transform.position.x + deltaX, -5.6f, 5.6f);
+        var newPosX = Mathf.Clamp(transform.position.x + deltaX, leftBoundary , rightBoundary);
 
         transform.position = new Vector2(newPosX, transform.position.y);
     }
